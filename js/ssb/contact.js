8966bc46a4f1d2ee4c70284fc59d547f8b98a1e3
Your Name <ingkongshi@163.com>
2017-11-09 08:57:33 +0800

    finall

--- js/ssb/contact.js
+++ js/ssb/contact.js
@@ -1,16 +1,22 @@
-$(function(){
-	$('.contact input').focus(function(){
-		$(this).css({border:"3px solid #e82281"})
-	})
-	$('.contact input').blur(function(){
-		$(this).css({border:"3px solid #ffc9e3"})
-	})
-	$('.contact #three').focus(function(){
-		$(this).css({border:"3px solid #e82281"})
-	})
-	$('.contact #three').blur(function(){
-		$(this).css({border:"3px solid #ffc9e3"})
-	})
+window.onload=function(){
 
 
-})
+	
+	var input=document.getElementsByClassName("put");
+	for(var i=0;i<input.length;i++){
+		input[i].onfocus=function(){
+			this.style.border="2px solid #FF3795"
+		}
+		input[i].onblur=function(){
+			this.style.border="2px solid #FFC9E3"
+		}
+	}
+
+
+}
+
+
+
+
+
+
--- js/zd/category.js
+++ js/zd/category.js
@@ -7,7 +7,28 @@ var lines=document.querySelectorAll('.liness');
 var zfxs=document.querySelectorAll('.zfx');
 
 	
-for(let i=0;i<imgbox.length;i++){
+// Array.from(imgbox).forEach(function(val,index){
+// 		val.onmouseenter=function(){
+// 		zfx[index].style.display="block";
+// 		font[index].style.cssText='background:rgb(252,24,115);position:absolute;left:-50px;top:0;width:210px';	
+// 		mask[index].style.cssText='border-bottom: 40px solid rgb(252,24,115);';
+// 		lines[index].style.cssText='left:65px;';
+// 		zfxs[index].style.cssText='left:125px';
+// 		sjx[index].style.display="none";
+	
+					
+// 		}
+// 		val.onmouseleave=function(){
+// 		sjx[index].style.display="block";
+// 		zfx[index].style.display="none";
+// 		font[index].style.cssText='background:rgb(153,153,153);width:160px';	
+// 		mask[index].style.cssText='border-bottom: 40px solid rgb(153,153,153);';
+// 		lines[index].style.cssText='left:40px;';
+// 		zfxs[index].style.cssText='left:100px';	
+// 		}
+// 		}
+// 		)
+	for(let i=0;i<imgbox.length;i++){
 		imgbox[i].onmouseenter=function(){
 			for(var j=0;j<imgbox.length;j++){
 				sjx[j].style.display="block";
@@ -17,7 +38,7 @@ for(let i=0;i<imgbox.length;i++){
 				lines[j].style.cssText='left:40px;';
 				zfxs[j].style.cssText='left:100px';	
 			}
-			zfx[i].style.display="block";
+			zfx[i ].style.display="block";
 			font[i].style.cssText='background:rgb(252,24,115);position:absolute;left:-50px;top:0;width:210px';	
 			mask[i].style.cssText='border-bottom: 40px solid rgb(252,24,115);';
 			lines[i].style.cssText='left:65px;';
@@ -32,4 +53,4 @@ for(let i=0;i<imgbox.length;i++){
 		lines[i].style.cssText='left:40px;';
 		zfxs[i].style.cssText='left:100px';	
 		}
-	}
+	}	
