b4232865b16bbacddc107c51e6d3d8358e8da1db
Blade-L <1017979119@qq.com>
2017-11-09 08:30:59 +0800

    aa

--- js/lb/index.js
+++ js/lb/index.js
@@ -1,3 +1,18 @@
 $(function(){
-	
+	var mySwiper2 = new Swiper('.swiper-container2',{
+	slidesPerView : 3,
+	slidesPerGroup : 3,
+	pagination : '.swiper-pagination1',
+	})
+	var mySwiper3 = new Swiper('.swiper-container3',{
+		pagination:'.swiper-pagination3',
+		paginationClickable :true,
+
+	})
+	var mySwiper4 = new Swiper('.swiper-container4',{
+		pagination:'.swiper-pagination4',
+		paginationClickable :true,
+		prevButton:'.swiper-button-prev',
+		nextButton:'.swiper-button-next',
+	})
 })
